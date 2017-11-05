3c8d277826a564b47036520a58782838c65b10bb
pcosel <casym.oo@web.de>
2017-11-05 15:19:55 +0100

    WIP: added json

--- code/main.js
+++ code/main.js
@@ -1,38 +1,429 @@
-/* var request;
 
-if(window.XMLHttpRequest) {
-    request = new XMLHttpRequest();
-} else {
-    request = new ActiveXObject("Microsoft.XMLHTTP");
+
+var json = "[\n" +
+    "  {\n" +
+    "    \"id\": 1,\n" +
+    "    \"name\": \"Brazil\",\n" +
+    "    \"birth rate per 1000\": 16.405,\n" +
+    "    \"cell phones per 100\": 90.01936334,\n" +
+    "    \"children per woman\": 1.862,\n" +
+    "    \"electricity consumption per capita\": 2201.808724,\n" +
+    "    \"gdp_per_capita\": 4424.758692,\n" +
+    "    \"gdp_per_capita_growth\": -1.520402823,\n" +
+    "    \"inflation annual\": 8.228535058,\n" +
+    "    \"internet user per 100\": 39.22,\n" +
+    "    \"life expectancy\": 74,\n" +
+    "    \"military expenditure percent of gdp\": 1.615173655,\n" +
+    "    \"gps_lat\": -14.235004,\n" +
+    "    \"gps_long\": -51.92528\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 2,\n" +
+    "    \"name\": \"Canada\",\n" +
+    "    \"birth rate per 1000\": 10.625,\n" +
+    "    \"cell phones per 100\": 70.70997244,\n" +
+    "    \"children per woman\": 1.668,\n" +
+    "    \"electricity consumption per capita\": 15119.76414,\n" +
+    "    \"gdp_per_capita\": 25069.86915,\n" +
+    "    \"gdp_per_capita_growth\": -3.953353186,\n" +
+    "    \"inflation annual\": 2.944408564,\n" +
+    "    \"internet user per 100\": 80.17086651,\n" +
+    "    \"life expectancy\": 80.9,\n" +
+    "    \"military expenditure percent of gdp\": 1.415710422,\n" +
+    "    \"gps_lat\": 56.130366,\n" +
+    "    \"gps_long\": -106.346771\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 3,\n" +
+    "    \"name\": \"Chile\",\n" +
+    "    \"birth rate per 1000\": 15.04,\n" +
+    "    \"cell phones per 100\": 97.01862561,\n" +
+    "    \"children per woman\": 1.873,\n" +
+    "    \"electricity consumption per capita\": 3276.06449,\n" +
+    "    \"gdp_per_capita\": 6451.631126,\n" +
+    "    \"gdp_per_capita_growth\": -2.610485847,\n" +
+    "    \"inflation annual\": 7.47050527,\n" +
+    "    \"internet user per 100\": 38.8,\n" +
+    "    \"life expectancy\": 78.8,\n" +
+    "    \"military expenditure percent of gdp\": 3.064076139,\n" +
+    "    \"gps_lat\": -35.675147,\n" +
+    "    \"gps_long\": 71.542969\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 4,\n" +
+    "    \"name\": \"China\",\n" +
+    "    \"birth rate per 1000\": 13.536,\n" +
+    "    \"cell phones per 100\": 55.97490921,\n" +
+    "    \"children per woman\": 1.642,\n" +
+    "    \"electricity consumption per capita\": 2632.724637,\n" +
+    "    \"gdp_per_capita\": 2208.403948,\n" +
+    "    \"gdp_per_capita_growth\": 8.648414427,\n" +
+    "    \"inflation annual\": 6.684109668,\n" +
+    "    \"internet user per 100\": 28.97659939,\n" +
+    "    \"life expectancy\": 75.6,\n" +
+    "    \"military expenditure percent of gdp\": 2.24110794,\n" +
+    "    \"gps_lat\": 35.86166,\n" +
+    "    \"gps_long\": 104.195397\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 5,\n" +
+    "    \"name\": \"Colombia\",\n" +
+    "    \"birth rate per 1000\": 20.605,\n" +
+    "    \"cell phones per 100\": 92.34584564,\n" +
+    "    \"children per woman\": 2.405,\n" +
+    "    \"electricity consumption per capita\": 1041.994137,\n" +
+    "    \"gdp_per_capita\": 3137.695335,\n" +
+    "    \"gdp_per_capita_growth\": 0.2081538559,\n" +
+    "    \"inflation annual\": 3.666941163,\n" +
+    "    \"internet user per 100\": 30,\n" +
+    "    \"life expectancy\": 75.3,\n" +
+    "    \"military expenditure percent of gdp\": 3.833780372,\n" +
+    "    \"gps_lat\": 4.570868,\n" +
+    "    \"gps_long\": -74.297333\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 6,\n" +
+    "    \"name\": \"Ecuador\",\n" +
+    "    \"birth rate per 1000\": 20.989,\n" +
+    "    \"cell phones per 100\": 92.84925653,\n" +
+    "    \"children per woman\": 2.69,\n" +
+    "    \"electricity consumption per capita\": 1078.038961,\n" +
+    "    \"gdp_per_capita\": 1692.067197,\n" +
+    "    \"gdp_per_capita_growth\": -1.079538461,\n" +
+    "    \"inflation annual\": 7.595866872,\n" +
+    "    \"internet user per 100\": 24.6,\n" +
+    "    \"life expectancy\": 74.1,\n" +
+    "    \"military expenditure percent of gdp\": 3.746501879,\n" +
+    "    \"gps_lat\": -1.831239,\n" +
+    "    \"gps_long\": -78.183406\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 7,\n" +
+    "    \"name\": \"Egypt\",\n" +
+    "    \"birth rate per 1000\": 24.83,\n" +
+    "    \"cell phones per 100\": 69.43661504,\n" +
+    "    \"children per woman\": 2.919,\n" +
+    "    \"electricity consumption per capita\": 1607.918763,\n" +
+    "    \"gdp_per_capita\": 1911.964501,\n" +
+    "    \"gdp_per_capita_growth\": 2.856917581,\n" +
+    "    \"inflation annual\": 10.10750976,\n" +
+    "    \"internet user per 100\": 24.28,\n" +
+    "    \"life expectancy\": 70.2,\n" +
+    "    \"military expenditure percent of gdp\": 2.137305699,\n" +
+    "    \"gps_lat\": 26.820553,\n" +
+    "    \"gps_long\": 30.802498\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 8,\n" +
+    "    \"name\": \"Finland\",\n" +
+    "    \"birth rate per 1000\": 11.127,\n" +
+    "    \"cell phones per 100\": 144.1530224,\n" +
+    "    \"children per woman\": 1.86,\n" +
+    "    \"electricity consumption per capita\": 15241.61194,\n" +
+    "    \"gdp_per_capita\": 26205.68832,\n" +
+    "    \"gdp_per_capita_growth\": -8.791558776,\n" +
+    "    \"inflation annual\": 0.4277308694,\n" +
+    "    \"internet user per 100\": 82.53133098,\n" +
+    "    \"life expectancy\": 79.7,\n" +
+    "    \"military expenditure percent of gdp\": 1.501872268,\n" +
+    "    \"gps_lat\": 61.92411,\n" +
+    "    \"gps_long\": 25.748151\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 9,\n" +
+    "    \"name\": \"France\",\n" +
+    "    \"birth rate per 1000\": 12.21,\n" +
+    "    \"cell phones per 100\": 95.44434226,\n" +
+    "    \"children per woman\": 1.978,\n" +
+    "    \"electricity consumption per capita\": 7339.946832,\n" +
+    "    \"gdp_per_capita\": 22508.76261,\n" +
+    "    \"gdp_per_capita_growth\": -3.264056248,\n" +
+    "    \"inflation annual\": 1.050366124,\n" +
+    "    \"internet user per 100\": 69.0633593,\n" +
+    "    \"life expectancy\": 81,\n" +
+    "    \"military expenditure percent of gdp\": 2.55313249,\n" +
+    "    \"gps_lat\": 46.227638,\n" +
+    "    \"gps_long\": 2.213749\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 10,\n" +
+    "    \"name\": \"Germany\",\n" +
+    "    \"birth rate per 1000\": 8.136,\n" +
+    "    \"cell phones per 100\": 127.4188883,\n" +
+    "    \"children per woman\": 1.376,\n" +
+    "    \"electricity consumption per capita\": 6753.05764,\n" +
+    "    \"gdp_per_capita\": 24368.19561,\n" +
+    "    \"gdp_per_capita_growth\": -4.886323581,\n" +
+    "    \"inflation annual\": 0.5959234322,\n" +
+    "    \"internet user per 100\": 79.48523153,\n" +
+    "    \"life expectancy\": 80,\n" +
+    "    \"military expenditure percent of gdp\": 1.438871341,\n" +
+    "    \"gps_lat\": 51.165691,\n" +
+    "    \"gps_long\": 10.451526\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 11,\n" +
+    "    \"name\": \"Iceland\",\n" +
+    "    \"birth rate per 1000\": 14.738,\n" +
+    "    \"cell phones per 100\": 107.6604456,\n" +
+    "    \"children per woman\": 2.123,\n" +
+    "    \"electricity consumption per capita\": 51259.18763,\n" +
+    "    \"gdp_per_capita\": 35310.97441,\n" +
+    "    \"gdp_per_capita_growth\": -6.990418561,\n" +
+    "    \"inflation annual\": 6.900703626,\n" +
+    "    \"internet user per 100\": 92.13686385,\n" +
+    "    \"life expectancy\": 82.2,\n" +
+    "    \"military expenditure percent of gdp\": 0.0820538039,\n" +
+    "    \"gps_lat\": 64.963051,\n" +
+    "    \"gps_long\": -19.020835\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 12,\n" +
+    "    \"name\": \"Iraq\",\n" +
+    "    \"birth rate per 1000\": 31.585,\n" +
+    "    \"cell phones per 100\": 65.47478839,\n" +
+    "    \"children per woman\": 4.276,\n" +
+    "    \"electricity consumption per capita\": 1086.323768,\n" +
+    "    \"gdp_per_capita\": 752.1833548,\n" +
+    "    \"gdp_per_capita_growth\": 1.141874289,\n" +
+    "    \"inflation annual\": 25.22442136,\n" +
+    "    \"internet user per 100\": 1.047516616,\n" +
+    "    \"life expectancy\": 68.1,\n" +
+    "    \"military expenditure percent of gdp\": 4.621383386,\n" +
+    "    \"gps_lat\": 33.223191,\n" +
+    "    \"gps_long\": 43.679291\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 13,\n" +
+    "    \"name\": \"Japan\",\n" +
+    "    \"birth rate per 1000\": 8.201,\n" +
+    "    \"cell phones per 100\": 91.8955442,\n" +
+    "    \"children per woman\": 1.359,\n" +
+    "    \"electricity consumption per capita\": 7838.005685,\n" +
+    "    \"gdp_per_capita\": 38242.02429,\n" +
+    "    \"gdp_per_capita_growth\": -6.180260885,\n" +
+    "    \"inflation annual\": -2.08543109,\n" +
+    "    \"internet user per 100\": 77.38468963,\n" +
+    "    \"life expectancy\": 82.8,\n" +
+    "    \"military expenditure percent of gdp\": 1.019445017,\n" +
+    "    \"gps_lat\": 36.204824,\n" +
+    "    \"gps_long\": 138.252924\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 14,\n" +
+    "    \"name\": \"Kazakhstan\",\n" +
+    "    \"birth rate per 1000\": 19.775,\n" +
+    "    \"cell phones per 100\": 107.7147692,\n" +
+    "    \"children per woman\": 2.537,\n" +
+    "    \"electricity consumption per capita\": 4447.142293,\n" +
+    "    \"gdp_per_capita\": 2345.86415,\n" +
+    "    \"gdp_per_capita_growth\": -1.437812068,\n" +
+    "    \"inflation annual\": 19.5422854,\n" +
+    "    \"internet user per 100\": 17.91457965,\n" +
+    "    \"life expectancy\": 66.6,\n" +
+    "    \"military expenditure percent of gdp\": 1.105385125,\n" +
+    "    \"gps_lat\": 48.019573,\n" +
+    "    \"gps_long\": 66.923684\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 15,\n" +
+    "    \"name\": \"Mexico\",\n" +
+    "    \"birth rate per 1000\": 19.091,\n" +
+    "    \"cell phones per 100\": 74.25785259,\n" +
+    "    \"children per woman\": 2.313,\n" +
+    "    \"electricity consumption per capita\": 1869.82352,\n" +
+    "    \"gdp_per_capita\": 5875.619997,\n" +
+    "    \"gdp_per_capita_growth\": -7.417438847,\n" +
+    "    \"inflation annual\": 4.033645258,\n" +
+    "    \"internet user per 100\": 26.34,\n" +
+    "    \"life expectancy\": 75.5,\n" +
+    "    \"military expenditure percent of gdp\": 0.5396656609,\n" +
+    "    \"gps_lat\": 23.634501,\n" +
+    "    \"gps_long\": -102.552784\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 16,\n" +
+    "    \"name\": \"New Zealand\",\n" +
+    "    \"birth rate per 1000\": 13.831,\n" +
+    "    \"cell phones per 100\": 108.7301521,\n" +
+    "    \"children per woman\": 2.125,\n" +
+    "    \"electricity consumption per capita\": 9375.550304,\n" +
+    "    \"gdp_per_capita\": 14778.16393,\n" +
+    "    \"gdp_per_capita_growth\": -1.552308788,\n" +
+    "    \"inflation annual\": 3.486782259,\n" +
+    "    \"internet user per 100\": 79.82609287,\n" +
+    "    \"life expectancy\": 80.3,\n" +
+    "    \"military expenditure percent of gdp\": 1.140562366,\n" +
+    "    \"gps_lat\": -40.900557,\n" +
+    "    \"gps_long\": 174.885971\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 17,\n" +
+    "    \"name\": \"Nigeria\",\n" +
+    "    \"birth rate per 1000\": 40.134,\n" +
+    "    \"cell phones per 100\": 48.23561006,\n" +
+    "    \"children per woman\": 6.021,\n" +
+    "    \"electricity consumption per capita\": 119.8151486,\n" +
+    "    \"gdp_per_capita\": 513.5003377,\n" +
+    "    \"gdp_per_capita_growth\": 4.3526073,\n" +
+    "    \"inflation annual\": 9.313146383,\n" +
+    "    \"internet user per 100\": 20,\n" +
+    "    \"life expectancy\": 58.5,\n" +
+    "    \"military expenditure percent of gdp\": 0.8924302789,\n" +
+    "    \"gps_lat\": 9.081999,\n" +
+    "    \"gps_long\": 8.675277\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 18,\n" +
+    "    \"name\": \"Peru\",\n" +
+    "    \"birth rate per 1000\": 21.342,\n" +
+    "    \"cell phones per 100\": 85.86901405,\n" +
+    "    \"children per woman\": 2.545,\n" +
+    "    \"electricity consumption per capita\": 1043.052601,\n" +
+    "    \"gdp_per_capita\": 2955.186222,\n" +
+    "    \"gdp_per_capita_growth\": -0.2228977721,\n" +
+    "    \"inflation annual\": 4.49134447,\n" +
+    "    \"internet user per 100\": 31.4,\n" +
+    "    \"life expectancy\": 76.7,\n" +
+    "    \"military expenditure percent of gdp\": 1.348875763,\n" +
+    "    \"gps_lat\": -9.189967,\n" +
+    "    \"gps_long\": -75.015152\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 19,\n" +
+    "    \"name\": \"Russia\",\n" +
+    "    \"birth rate per 1000\": 10.828,\n" +
+    "    \"cell phones per 100\": 161.1162887,\n" +
+    "    \"children per woman\": 1.537,\n" +
+    "    \"electricity consumption per capita\": 6132.978648,\n" +
+    "    \"gdp_per_capita\": 2806.41483,\n" +
+    "    \"gdp_per_capita_growth\": -7.749103694,\n" +
+    "    \"inflation annual\": 11.60398093,\n" +
+    "    \"internet user per 100\": 29.23584146,\n" +
+    "    \"life expectancy\": 68.3,\n" +
+    "    \"military expenditure percent of gdp\": 4.36259042,\n" +
+    "    \"gps_lat\": 61.52401,\n" +
+    "    \"gps_long\": 105.318756\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 20,\n" +
+    "    \"name\": \"Saudi Arabia\",\n" +
+    "    \"birth rate per 1000\": 23.569,\n" +
+    "    \"cell phones per 100\": 167.3474553,\n" +
+    "    \"children per woman\": 2.898,\n" +
+    "    \"electricity consumption per capita\": 7430.743897,\n" +
+    "    \"gdp_per_capita\": 9294.355996,\n" +
+    "    \"gdp_per_capita_growth\": -2.242127204,\n" +
+    "    \"inflation annual\": 14.36222827,\n" +
+    "    \"internet user per 100\": 38,\n" +
+    "    \"life expectancy\": 77.6,\n" +
+    "    \"military expenditure percent of gdp\": 10.95653405,\n" +
+    "    \"gps_lat\": 23.885942,\n" +
+    "    \"gps_long\": 45.079162\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 21,\n" +
+    "    \"name\": \"South Africa\",\n" +
+    "    \"birth rate per 1000\": 22.113,\n" +
+    "    \"cell phones per 100\": 93.33587369,\n" +
+    "    \"children per woman\": 2.5,\n" +
+    "    \"electricity consumption per capita\": 4532.021902,\n" +
+    "    \"gdp_per_capita\": 3697.67368,\n" +
+    "    \"gdp_per_capita_growth\": -2.732989408,\n" +
+    "    \"inflation annual\": 7.861608575,\n" +
+    "    \"internet user per 100\": 10.08745979,\n" +
+    "    \"life expectancy\": 55.8,\n" +
+    "    \"military expenditure percent of gdp\": 1.394530379,\n" +
+    "    \"gps_lat\": -30.559482,\n" +
+    "    \"gps_long\": 22.937506\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 22,\n" +
+    "    \"name\": \"Sweden\",\n" +
+    "    \"birth rate per 1000\": 11.72,\n" +
+    "    \"cell phones per 100\": 112.1241184,\n" +
+    "    \"children per woman\": 1.937,\n" +
+    "    \"electricity consumption per capita\": 14143.01101,\n" +
+    "    \"gdp_per_capita\": 30885.45914,\n" +
+    "    \"gdp_per_capita_growth\": -5.976535294,\n" +
+    "    \"inflation annual\": 1.022227082,\n" +
+    "    \"internet user per 100\": 91.12326108,\n" +
+    "    \"life expectancy\": 81.2,\n" +
+    "    \"military expenditure percent of gdp\": 1.247701873,\n" +
+    "    \"gps_lat\": 60.128161,\n" +
+    "    \"gps_long\": 18.643501\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 23,\n" +
+    "    \"name\": \"United Arab Emirates\",\n" +
+    "    \"birth rate per 1000\": 14.027,\n" +
+    "    \"cell phones per 100\": 153.7997194,\n" +
+    "    \"children per woman\": 1.903,\n" +
+    "    \"electricity consumption per capita\": 9998.291079,\n" +
+    "    \"gdp_per_capita\": 22507.00157,\n" +
+    "    \"gdp_per_capita_growth\": -11.99171952,\n" +
+    "    \"inflation annual\": 8.549032358,\n" +
+    "    \"internet user per 100\": 64,\n" +
+    "    \"life expectancy\": 76.1,\n" +
+    "    \"military expenditure percent of gdp\": 5.834881976,\n" +
+    "    \"gps_lat\": 23.424076,\n" +
+    "    \"gps_long\": 53.847818\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 24,\n" +
+    "    \"name\": \"United Kingdom\",\n" +
+    "    \"birth rate per 1000\": 12.195,\n" +
+    "    \"cell phones per 100\": 130.1742603,\n" +
+    "    \"children per woman\": 1.89,\n" +
+    "    \"electricity consumption per capita\": 5685.635995,\n" +
+    "    \"gdp_per_capita\": 27933.77767,\n" +
+    "    \"gdp_per_capita_growth\": -5.019251823,\n" +
+    "    \"inflation annual\": 2.861406642,\n" +
+    "    \"internet user per 100\": 77.79971962,\n" +
+    "    \"life expectancy\": 79.7,\n" +
+    "    \"military expenditure percent of gdp\": 2.667209048,\n" +
+    "    \"gps_lat\": 52.3555177,\n" +
+    "    \"gps_long\": -1.1743197\n" +
+    "  },\n" +
+    "  {\n" +
+    "    \"id\": 25,\n" +
+    "    \"name\": \"United States\",\n" +
+    "    \"birth rate per 1000\": 14.191,\n" +
+    "    \"cell phones per 100\": 89.14911634,\n" +
+    "    \"children per woman\": 2.002,\n" +
+    "    \"electricity consumption per capita\": 12913.71143,\n" +
+    "    \"gdp_per_capita\": 36539.22823,\n" +
+    "    \"gdp_per_capita_growth\": -4.342271218,\n" +
+    "    \"inflation annual\": 1.152326348,\n" +
+    "    \"internet user per 100\": 71.21181627,\n" +
+    "    \"life expectancy\": 78.3,\n" +
+    "    \"military expenditure percent of gdp\": 4.822730027,\n" +
+    "    \"gps_lat\": 37.09024,\n" +
+    "    \"gps_long\": -95.712891\n" +
+    "  }\n" +
+    "]\n";
+
+var items = JSON.parse(json);
+
+console.log(items);
+
+var output = '   <table>\n' +
+    '        <tr>\n' +
+    '            <th>ID</th>\n' +
+    '            <th>Country</th>\n' +
+    '            <th class="birth_rate">birth rate / 1000</th>\n' +
+    '            <th class="cellphones">cellphones / 100</th>\n' +
+    '            <th class="children">children / women</th>\n' +
+    '            <th class="electric_usage">electric usage</th>\n' +
+    '            <th class="internet_usage">internet usage</th>\n' +
+    '        </tr>';
+
+output += '<tr>';
+
+for (var key in items) {
+    output += '<td>' + items[key].b + '</td>';
 }
 
-request.open('GET', '../world_data_v1.json');
-request.onreadystatechange = function () {
-    if((request.readyState === 4) && request.status === 200) {
-        var items = JSON.parse(request.responseText);
-        console.log(items);
-    }
-}
+output += '</tr>';
 
-request.send(); */
-
-var text = "[\n" +
-    "   {\n" +
-    "      \"id \": \"001\",\n" +
-    "      \"name\": \"Brazil\",\n" +
-    "      \"birth rate per 1000\": \"16.405\",\n" +
-    "      \"cell phones per 100\": \"90.01936334\",\n" +
-    "      \"children per woman\": \"1.862\",\n" +
-    "      \"electricity consumption per capita\": \"2201.808724\",\n" +
-    "      \"gdp_per_capita\": \"4424.758692\",\n" +
-    "      \"gdp_per_capita_growth\": \"-1.520402823\",\n" +
-    "      \"inflation annual\": \"8.228535058\",\n" +
-    "      \"internet user per 100\": \"39.22\",\n" +
-    "      \"life expectancy\": \"74\",\n" +
-    "      \"military expenditure percent of gdp\": \"1.615173655\",\n" +
-    "      \"gps_lat\": -14.235004,\n" +
-    "      \"gps_long\": -51.92528\n" +
-    "   }]";
-var result = JSON.parse(text);
-
-console.log(result);
+document.write(output);
