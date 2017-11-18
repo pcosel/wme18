7eecc65664f15068e6f271d603d13517c8887859
pcosel <casymoo@web.de>
2017-11-18 15:40:13 +0100

    save finished

--- code/PHP/save.php
+++ code/PHP/save.php
@@ -2,4 +2,13 @@
 
     require("world_data_parser.php");
 
+    $array = parseCSV("../../world_data_v1.csv");
+
+    print("XML Savestatus: ");
+
+    if(saveXML($array)) {
+        print("erfolgreich (1)");
+    } else {
+        print("fehlerhaft(0)");
+    }
 ?>
--- code/PHP/world_data_parser.php
+++ code/PHP/world_data_parser.php
@@ -4,7 +4,9 @@
             $array = array();
             $innerArray = array();
             $firstLine = fgetcsv($file);
-
+            foreach ($firstLine as $key => $value) {
+                $firstLine[$key] = str_replace(" ", "_", $value);
+            }
             $i = 0;
             while(!feof($file)) {
                 $line = fgetcsv($file);
@@ -18,8 +20,29 @@
             return $array;
         }
 
-        function saveXML () {
+        function saveXML (array $array) {
+            $xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Countries></Countries>");
 
+            foreach ($array as $key1 => $value1) {
+                $xmlChild = $xml -> addChild("Country");
+                foreach ($value1 as $key2 => $value2) {
+                    $xmlChild -> addChild("$key2", htmlspecialchars("$value2"));
+                }
+            }
+
+            print($xml);
+
+            $result = $xml -> asXML("world_data.xml");
+
+            return $result;
+
+            /*$file = fopen("world_data.xml", "w");
+            $error = fwrite($file, $xml);
+            if($error == false) {
+                return true;
+            } else {
+                return false;
+            } */
         }
 
         function printXML () {
