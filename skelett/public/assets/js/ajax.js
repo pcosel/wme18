5b291d3251313d520f3a92f99a978b012258b71d
yasminhaustein <yasmin.haustein@mailbox.tu-dresden.de>
2017-12-11 16:16:03 +0100

    rendering table outsourced

--- skelett/public/assets/js/ajax.js
+++ skelett/public/assets/js/ajax.js
@@ -4,26 +4,27 @@ $.ajax({
     url: "http://localhost:3000/items",
     async: true,
     success: function(data) {
-console.log(data);
-        var output = "";
-    for (var key in data) {
-        item = data[key];
-        output+=
-        ' <tr>' +
-        ' <td class="id">' + item.id + '</td>' +
-        ' <td class="name">' + item.name + '</td>' +
-        ' <td class="birth_rate">' + item["birth rate per 1000"] + '</td>' +
-        ' <td class="cellphones">' + item["cell phones per 100"] + '</td>' +
-        ' <td class="children">' + item["children per woman"] + '</td>' +
-        ' <td class="electric_usage">' + item["electricity consumption per capita"] + '</td>' +
-        ' <td class="internet_usage">' + item["internet user per 100"] + '</td>' +
-        ' </tr>';
-    }
-    console.log (output);
-    //console.log ($("#table-body"));
-        $('#table_body').html(output);
-
+    displayTable(data);
     }, error: function(jqXHR, text, err) {
 // Handle error if occured
     }
 });
+
+function displayTable (data) {
+    var output = "";
+    for (var key in data) {
+        item = data[key];
+        output+=
+            ' <tr>' +
+            ' <td class="id">' + item.id + '</td>' +
+            ' <td class="name">' + item.name + '</td>' +
+            ' <td class="birth_rate">' + item["birth rate per 1000"] + '</td>' +
+            ' <td class="cellphones">' + item["cell phones per 100"] + '</td>' +
+            ' <td class="children">' + item["children per woman"] + '</td>' +
+            ' <td class="electric_usage">' + item["electricity consumption per capita"] + '</td>' +
+            ' <td class="internet_usage">' + item["internet user per 100"] + '</td>' +
+            ' </tr>';
+    }
+    $('#table_body').html(output);
+
+}
